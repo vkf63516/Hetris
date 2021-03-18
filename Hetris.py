feac773f236238a08611d91f43506ef533ef2f50
Vishal Fenn <56054927+vkf63516@users.noreply.github.com>
2021-03-17 22:13:00 -0400

    Modified tetris to go sideways

---
+++ Hetris.py
@@ -0,0 +1,218 @@
+import pygame, sys
+from pygame.locals import *
+
+import random
+
+colors = [
+    (0, 0, 0),
+    (120, 37, 165),
+    (100, 150, 150),
+    (60, 44, 22),
+    (69, 144, 22),
+    (160, 44, 22),
+    (160, 44, 122)
+    ]
+
+class Shapes: 
+    x = 0
+    y = 0
+
+    figures = [
+    [[1, 5, 9, 13], [4, 5, 6, 7]], # Straight block
+    [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]], # reverse L block
+    [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]], # L block
+    [[1, 4, 5, 6], [4, 9, 5, 1], [9, 4, 5, 6], [6, 1, 5, 9]], # T block
+    [[0, 1, 5, 6], [8, 4, 5, 1]], # Left Skew block
+    [[2, 1, 5, 4], [0, 4, 5, 9]], # Right Skew block
+    [[1, 2, 5, 6]], #Square block
+    ]
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.type = random.randint(0, len(self.figures) - 1)
+        self.color = random.randint(1, len(colors) - 1)
+        self.rotation = 0
+
+    def image(self):
+        return self.figures[self.type][self.rotation]
+
+    def rotate(self):
+        self.rotation = (self.rotation + 1) % len(self.figures[self.type])
+
+class Hetris:
+    level = 2
+    score = 0
+    state = 'start'
+    field = []
+    height = 0
+    width = 0
+    x = 100
+    y = 60
+    zoom = 12
+    figure = None
+
+    def __init__(self, height, width):
+
+        self.height = height
+        self.width = width
+        self.field = []
+        self.score = 0
+        self.state = "start"
+        for i in range(width):
+            new_line = []
+            for j in range(height):
+                new_line.append(0)
+            self.field.append(new_line)
+    def new_figure(self): 
+        self.figure = Shapes(0, 5)
+
+    def intersects(self):
+        intersection = False
+        for i in range(4):
+            for j in range(4):
+                if i * 4 + j in self.figure.image():
+                    if i + self.figure.y > self.width - 1 or \
+                            j + self.figure.x > self.height - 1 or \
+                            j + self.figure.x < 0 or \
+                            self.field[i + self.figure.y][j + self.figure.x] > 0:
+                        intersection = True
+        return intersection
+
+    def break_lines(self):
+        lines = 0
+        for i in range(1, self.width):
+            zeros = 0
+            for j in range(self.height):
+                if self.field[i][j] == 0:
+                    zeros += 1
+            if zeros == 0:
+                lines += 1
+                for i1 in range(j, 1, -1):
+                    for j in range(self.height):
+                        self.field[i1][j] = self.field[i1-1][j]
+        self.score += lines ** 2
+
+    def go_space(self):
+        while not self.intersects():
+            self.figure.x += 1
+        self.figure.x -= 1
+        self.freeze()
+
+    def go_right(self):
+        self.figure.x += 1
+        if self.intersects():
+            self.figure.x -= 1
+            self.freeze()
+
+    def freeze(self):
+        for i in range(4):
+            for j in range(4):
+                if i * 4 + j in self.figure.image():
+                    self.field[i + self.figure.y][j + self.figure.x] = self.figure.color
+        self.break_lines()
+        self.new_figure()
+        if self.intersects():
+            self.state = "gameover"
+
+    def go_side(self, dx):
+        old_x = self.figure.y
+        self.figure.y += dx
+        if self.intersects():
+            self.figure.y = old_x
+
+    def rotate(self):
+        old_rotation = self.figure.rotation
+        self.figure.rotate()
+        if self.intersects():
+            self.figure.rotation = old_rotation
+
+
+# Initialize the game engine
+pygame.init()
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
+
+size = (400, 500)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Hetris")
+
+# Loop until the user clicks the close button.
+done = False
+clock = pygame.time.Clock()
+fps = 25
+game = Hetris(20, 10)
+counter = 0
+
+pressing_down = False
+
+while not done:
+    if game.figure is None:
+        game.new_figure()
+    counter += 1
+    if counter > 100000:
+        counter = 0
+
+    if counter % (fps // game.level // 2) == 0 or pressing_down:
+        if game.state == "start":
+            game.go_right()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                game.go_side(-1)
+            if event.key == pygame.K_DOWN:
+                game.go_side(1)
+            if event.key == pygame.K_LEFT:
+                game.rotate()
+            if event.key == pygame.K_RIGHT:
+                game.rotate()
+            if event.key == pygame.K_SPACE:
+                game.go_space()
+            if event.key == pygame.K_ESCAPE:
+                game.__init__(20, 15)
+
+    if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                pressing_down = False
+
+    screen.fill(WHITE)
+
+    for i in range(game.width):
+        for j in range(game.height):
+            pygame.draw.rect(screen, GRAY, [game.x + game.zoom * j, game.y + game.zoom * i, game.zoom, game.zoom], 1)
+            if game.field[i][j] > 0:
+                pygame.draw.rect(screen, colors[game.field[i][j]],
+                                 [game.x + game.zoom * j + 1, game.y + game.zoom * i + 1, game.zoom - 2, game.zoom - 1])
+
+    if game.figure is not None:
+        for i in range(4):
+            for j in range(4):
+                p = i * 4 + j
+                if p in game.figure.image():
+                    pygame.draw.rect(screen, colors[game.figure.color],
+                                     [game.x + game.zoom * (j + game.figure.x) + 1,
+                                      game.y + game.zoom * (i + game.figure.y) + 1,
+                                      game.zoom - 2, game.zoom - 2])
+
+    font = pygame.font.SysFont('Calibri', 25, True, False)
+    font1 = pygame.font.SysFont('Calibri', 65, True, False)
+    text = font.render("Score: " + str(game.score), True, BLACK)
+    text_game_over = font1.render("Game Over", True, (255, 125, 0))
+    text_game_over1 = font1.render("Press ESC", True, (255, 215, 0))
+
+    screen.blit(text, [0, 0])
+    if game.state == "gameover":
+        screen.blit(text_game_over, [20, 200])
+        screen.blit(text_game_over1, [25, 265])
+
+    pygame.display.flip()
+    clock.tick(fps)
+
+pygame.quit()
